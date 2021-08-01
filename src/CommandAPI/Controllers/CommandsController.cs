20f8b25bf8237be77380d48e95777a6222c48ae2
raghav <rgv3300@gmail.com>
2021-08-01 06:32:37 -0400

    initial commit

---
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandAPI.Controllers
+{
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "this", "is", "test", "code" };
+        }
+    }
+}
